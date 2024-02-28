d653dbed5ef40bb98f87a6bf7be7a6f2174a3ec9
rainnthundr <145371191+rainnthundr@users.noreply.github.com>
2024-02-28 12:54:56 -0500

    Ready for grading

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int number = 30;
+
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.WriteLine(i + " x " + number + " = " + i * number);
+            }
+
+            Console.WriteLine("******");
+        }
+    }
+}
